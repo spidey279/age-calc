2e77fb9cca148173b32e954fe4c8af86980aae02
yyyaaassshh <yashpawar2995@gmail.com>
2025-07-05 03:26:21 +0530

    forgor

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ const umar = document.querySelector(".umar");
 const button = document.querySelector(".submit");
 
 button.addEventListener("click", () => {
-  const option = document.querySelector('input[name="choices"]:checked');
-  if (option.value === "male") {
-    const dobValue = dob.value;
-    const year = new Date(dobValue).getFullYear();
-    const currentYear = new Date().getFullYear();
-    const age = currentYear - year;
+  const dobValue = dob.value;
+  const year = new Date(dobValue).getFullYear();
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
 
-    umar.textContent = `Damn bro, you’re ${age}? That’s not old — that’s ancient`;
-  }
+  umar.textContent = `Damn bro, you’re ${age}? That’s not old — that’s ancient`;
 });
